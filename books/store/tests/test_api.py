ab1d08c70d542d125efc1bd5da56492cc9c8bee6
vadignat <vadimignat02@gmail.com>
2024-03-08 18:56:08 +0300

    Add filters, search and ordering

--- books/store/tests/test_api.py
+++ books/store/tests/test_api.py
@@ -7,13 +7,26 @@ from store.serializers import BooksSerializer
 
 
 class BooksApiTestCase(APITestCase):
+    def setUp(self):
+        self.book1 = Book.objects.create(name='Book 1', price=25,
+                                         author_name='Author1')
+        self.book2 = Book.objects.create(name='Book 2', price=55,
+                                         author_name='Author 5')
+        self.book3 = Book.objects.create(name='Book Author 1', price=55,
+                                         author_name='Author2')
+
     def test_get(self):
-        book1 = Book.objects.create(name="Book 1", price=25)
-        book2 = Book.objects.create(name="Book 2", price=55)
         url = reverse('book-list')
         response = self.client.get(url)
-        serializer_data = BooksSerializer([book1, book2], many=True).data
+        serializer_data = BooksSerializer([self.book1, self.book2,
+                                           self.book3], many=True).data
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(serializer_data, response.data)
 
-
+    def test_get_search(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'search': 'Author 1'})
+        serializer_data = BooksSerializer([self.book1,
+                                           self.book3], many=True).data
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(serializer_data, response.data)
